dfd76d6b1b284d637081dda4c521438720052825
programming6131 <kimutainashon@gmail.com>
2024-07-30 21:47:32 +0300

    search and search case sensitive words

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,5 +13,43 @@ func main() {
 	filename := args[2]
 	query := args[1]
 	fmt.Printf("Filename: %s\nQuery: %s", filename, query)
+	//search
 
+	results := search(filename, query)
+
+	fmt.Printf("results: %s\n", results)
+
+}
+
+func search(filename string, query string) []string {
+
+	contents, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Failed to read file", err)
+	}
+	fmt.Printf("Contents: %s\n", contents)
+	lines := strings.Split(string(contents), "\n")
+	var results []string
+	for _, line := range lines {
+		if strings.Contains(line, query) {
+			results = append(results, line)
+		}
+	}
+	return results
+}
+
+func SearchCaseSensitive(filename string, query string) []string {
+	query = strings.ToLower(query)
+	contents, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Failed to read file", err)
+	}
+	lines := strings.Split(string(contents), "\n")
+	var results []string
+	for _, line := range lines {
+		if strings.Contains(strings.ToLower(line), query) {
+			results = append(results, line)
+		}
+	}
+	return results
 }
